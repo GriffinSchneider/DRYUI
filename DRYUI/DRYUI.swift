aed232e6ea6c8454636fb391138c4a2fe5abf551
Griffin <griffin@Griffins-iMac.local>
2016-02-21 00:06:20 -0500

    Fix infinite recursion.

--- DRYUI/DRYUI.swift
+++ DRYUI/DRYUI.swift
@@ -9,17 +9,16 @@ import SnapKit
 #endif
 
 public protocol DRYUIAble {
-    func dryui_addSubview(view: UIView) -> Void
+    func dryui_addSubview(view: View) -> Void
 }
 
-extension UIView: DRYUIAble {
-    public func dryui_addSubview(view: UIView) {
+extension View: DRYUIAble {
+    public func dryui_addSubview(view: View) {
         addSubview(view)
     }
 }
 
 public extension DRYUIAble {
-    
     // MARK: Add a view with inferred type from blocks
     public func addSubview<SubviewType:View>(subviewBlocks: (SubviewType) -> Void ...) -> SubviewType {
         return addSubviewInternal(SubviewType(), subviewBlocks)
@@ -28,6 +27,7 @@ public extension DRYUIAble {
     public func addSubview<SubviewType:View>(subviewBlocks: (SubviewType) -> Void ..., @noescape _ lastBlock: (SubviewType, ConstraintMaker) -> Void) -> SubviewType {
         return addSubviewInternal(SubviewType(), subviewBlocks, lastBlock)
     }
+    
     
     // MARK: Add a view with type passed explicitly
     public func addSubview<SubviewType:View>(subviewType: SubviewType.Type, _ subviewBlocks: (SubviewType) -> Void ...) -> SubviewType {
@@ -38,6 +38,7 @@ public extension DRYUIAble {
         return addSubviewInternal(SubviewType(), subviewBlocks, lastBlock)
     }
     
+    
     // MARK: Add a view with the view instance passed explicitly
     public func addSubview<SubviewType:View>(subview: SubviewType, _ subviewBlocks: (SubviewType) -> Void ...) -> SubviewType {
         return addSubviewInternal(subview, subviewBlocks)
@@ -47,16 +48,17 @@ public extension DRYUIAble {
         return addSubviewInternal(subview, subviewBlocks, lastBlock)
     }
     
+    
     // MARK: Private methods
     // These 'internal' methods _should_ be just more overloads of addSubview, but doing it that way makes the compiler crash (XCode 7.2).
     private func addSubviewInternal<SubviewType:View>(subview: SubviewType, _ subviewBlocks: [(SubviewType) -> Void]) -> SubviewType {
-        self.addSubview(subview)
+        self.dryui_addSubview(subview)
         subviewBlocks.forEach{ $0(subview) }
         return subview
     }
     
     private func addSubviewInternal<SubviewType:View>(subview: SubviewType, _ subviewBlocks: [(SubviewType) -> Void], @noescape _ lastBlock: (SubviewType, ConstraintMaker) -> Void) -> SubviewType {
-        self.addSubview(subview)
+        self.dryui_addSubview(subview)
         subviewBlocks.forEach{ $0(subview) }
         subview.snp_makeConstraints { lastBlock(subview, $0) }
         return subview
