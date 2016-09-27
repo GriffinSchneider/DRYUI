ae0a52cbe1415556bcacb890990e647459fbe624
Griffin <griffinschneider@gmail.com>
2016-09-26 22:57:56 -0400

    [Swift] Rework protocol structure. Remove dryui_addSubview workaround.

--- DRYUI/DRYUI.swift
+++ DRYUI/DRYUI.swift
@@ -9,27 +9,26 @@ import SnapKit
 #endif
 
 public protocol DRYUIAble {
-    func dryui_addSubview(_ view: View) -> Void
+    @discardableResult func addSubview<SubviewType:View>(_ subviewBlocks: (SubviewType) -> Void ...) -> SubviewType
+    @discardableResult func addSubview<SubviewType:View>(_ subviewBlocks: (SubviewType) -> Void ..., _ lastBlock: (SubviewType, ConstraintMaker) -> Void) -> SubviewType
+    @discardableResult func addSubview<SubviewType:View>(_ subviewType: SubviewType.Type, _ subviewBlocks: (SubviewType) -> Void ...) -> SubviewType
+    @discardableResult func addSubview<SubviewType:View>(_ subviewType: SubviewType.Type, _ subviewBlocks: (SubviewType) -> Void ..., _ lastBlock: (SubviewType, ConstraintMaker) -> Void) -> SubviewType
+    @discardableResult func addSubview<SubviewType:View>(_ subview: SubviewType, _ subviewBlocks: (SubviewType) -> Void ...) -> SubviewType
+    @discardableResult func addSubview<SubviewType:View>(_ subview: SubviewType, _ subviewBlocks: (SubviewType) -> Void ..., _ lastBlock: (SubviewType, ConstraintMaker) -> Void) -> SubviewType
 }
 
 extension View: DRYUIAble {
-    public func dryui_addSubview(_ view: View) {
-        addSubview(view)
-    }
-}
-
-public extension DRYUIAble {
     // MARK: Add a view with inferred type from blocks
     @discardableResult public func addSubview<SubviewType:View>(_ subviewBlocks: (SubviewType) -> Void ...) -> SubviewType {
         let subview = SubviewType()
-        self.dryui_addSubview(subview)
+        addSubview(subview)
         subviewBlocks.forEach{ $0(subview) }
         return subview
     }
     
     @discardableResult public func addSubview<SubviewType:View>(_ subviewBlocks: (SubviewType) -> Void ..., _ lastBlock: (SubviewType, ConstraintMaker) -> Void) -> SubviewType {
         let subview = SubviewType()
-        self.dryui_addSubview(subview)
+        addSubview(subview)
         subviewBlocks.forEach{ $0(subview) }
         subview.snp.makeConstraints { lastBlock(subview, $0) }
         return subview
@@ -39,14 +38,14 @@ public extension DRYUIAble {
     // MARK: Add a view with type passed explicitly
     @discardableResult public func addSubview<SubviewType:View>(_ subviewType: SubviewType.Type, _ subviewBlocks: (SubviewType) -> Void ...) -> SubviewType {
         let subview = SubviewType()
-        self.dryui_addSubview(subview)
+        addSubview(subview)
         subviewBlocks.forEach{ $0(subview) }
         return subview
     }
     
     @discardableResult public func addSubview<SubviewType:View>(_ subviewType: SubviewType.Type, _ subviewBlocks: (SubviewType) -> Void ..., _ lastBlock: (SubviewType, ConstraintMaker) -> Void) -> SubviewType {
         let subview = SubviewType()
-        self.dryui_addSubview(subview)
+        addSubview(subview)
         subviewBlocks.forEach{ $0(subview) }
         subview.snp.makeConstraints { lastBlock(subview, $0) }
         return subview
@@ -55,13 +54,13 @@ public extension DRYUIAble {
     
     // MARK: Add a view with the view instance passed explicitly
     @discardableResult public func addSubview<SubviewType:View>(_ subview: SubviewType, _ subviewBlocks: (SubviewType) -> Void ...) -> SubviewType {
-        self.dryui_addSubview(subview)
+        addSubview(subview)
         subviewBlocks.forEach{ $0(subview) }
         return subview
     }
     
     @discardableResult public func addSubview<SubviewType:View>(_ subview: SubviewType, _ subviewBlocks: (SubviewType) -> Void ..., _ lastBlock: (SubviewType, ConstraintMaker) -> Void) -> SubviewType {
-        self.dryui_addSubview(subview)
+        addSubview(subview)
         subviewBlocks.forEach{ $0(subview) }
         subview.snp.makeConstraints { lastBlock(subview, $0) }
         return subview
